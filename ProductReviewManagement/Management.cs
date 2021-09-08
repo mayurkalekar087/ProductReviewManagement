1015ea7f6e77408702b01a23075aeea76421f61c
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-08 12:44:26 +0530

    Feat:Ability To Retrieve Selected Records

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -10,6 +10,7 @@ namespace ProductReviewManagement
     {
         public readonly DataTable dataTable = new DataTable();
 
+        //UC2
         public void TopRecords(List<ProductReview> listProductReview)
         {
             var recordedData = (from productReviews in listProductReview
@@ -21,6 +22,21 @@ namespace ProductReviewManagement
                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike :- " + list.isLike);
             }
         }
+        //UC3
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.ProductId == 1 ||
+                                     productReviews.ProductId == 4 ||
+                                     productReviews.ProductId == 9) && (productReviews.Rating > 3)
+                               select productReviews;
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId:- " + list.ProductId + " " + "UserId:- " + list.UserId
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike :- " + list.isLike);
+            }
+        }
     }
 }
-
+ 
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -29,12 +29,17 @@ namespace ProductReviewManagement
                 new ProductReview(){ProductId=15,UserId=10,Rating=4,Review="Nice",isLike=true },
                 new ProductReview(){ProductId=16,UserId=10,Rating=4,Review="Nice",isLike=true }
             };
-            foreach (var list in productReviewList)
-            {
-                Console.WriteLine("ProductId:- " + list.ProductId + " " + "UserId:- " + list.UserId
-                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike :- " + list.isLike);
-            }
+            //foreach (var list in productReviewList)
+            //{
+            //    Console.WriteLine("ProductId:- " + list.ProductId + " " + "UserId:- " + list.UserId
+            //        + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike :- " + list.isLike);
+            //}
+            Management management = new Management();
+            //UC2
+            management.TopRecords(productReviewList);
 
+            //UC3
+            management.SelectedRecords(productReviewList);
         }
     }
 }
